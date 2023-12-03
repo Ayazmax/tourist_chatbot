2384345ed836deadd2bf946f95636ce35cb1677e
Ayazmax <ayazm9582@gmail.com>
2023-12-03 14:34:42 +0530

    app.py changed

--- app.py
+++ app.py
@@ -81,11 +81,6 @@ app = Flask(__name__)
 app.static_folder = 'static'
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
